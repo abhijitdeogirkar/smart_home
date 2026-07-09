353cb163028356323cf5fd12e7220d1f2709685d
abhijitdeogirkar <70119313+abhijitdeogirkar@users.noreply.github.com>
2026-07-09 23:31:12 +0530

    Initialize global session states in app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+// सिस्टीमचे मुख्य ग्लोबल स्टेट्स (Global Session States)
+let states = {
+    ug_pump: false, 
+    bw1_pump: false, 
+    bw2_pump: false,
+    valve_t1: false, 
+    valve_t2: false, 
+    valve_ug: false,
+    sim_tanker: false, 
+    motion_detected: false, 
+    alarm_armed: false
+};
 
+// डॅशबोर्ड पूर्ण लोड झाल्यावर सर्व यंत्रणा सुरू करणे
+window.onload = function() {
+    initPanchang();
+    updateWaterFlowLogic();
+};
